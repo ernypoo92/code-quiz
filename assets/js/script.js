cce669df0216aefd7b42447a5e7145b0bcfe2332
Aaron Jones <ernypoo92@gmail.com>
2022-02-12 16:44:10 -0700

    generate question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,49 +12,48 @@ var name = document.getElementById("name");
 var highScores = document.getElementById("high-scores");
 var feedbackDiv = document.getElementById("feedback-div");
 var feedback = document.getElementById("feedback");
-
+var currentQuestionIndex = 0;
 
 // The array of questions, choices and correct answers.
 const questionsBank= [
-    {   q: 'Arrays in Javascript can be used to store __________.', 
-        a: '4. all of the above', 
+    {   question: 'Arrays in Javascript can be used to store __________.', 
+        answer: '4. all of the above', 
         choices: [{choice: '1. numbers'}, {choice: '2. booleans'}, {choice: '3. strings'}, {choice: '4. all of the above'}]
     },
-    {   q: 'Inside which HTML element do we put the javascript?', 
-        a: '3. <script>', 
-        choices: [{choice: '1. <h1>'}, {choice: '2. <js>'}, {choice: '3. <script>'}, {choice: '4. <head>'}]
+    {   question: 'Inside which HTML element do we put the javascript?', 
+        answer: '3. <script>', 
+        choices: [{choice: '1. <h1>'}, {choice: '2. <js>'}, {choice: '3. <script>'}, {choice: '4. <henswerd>'}]
     },
-    {   q: 'In the code -- setinterval(time(),1000) -- what is time()?', 
-        a: '1. callback function', 
+    {   question: 'In the code -- setinterval(time(),1000) -- what is time()?', 
+        answer: '1. callback function', 
         choices: [{choice: '1. callback function'}, {choice: '2. undefined'}, {choice: '3. variable'}, {choice: '4. all of the above'}]
     },
-    {   q: 'What syntax would call a function?', 
-        a: '4. function()', 
+    {   question: 'What syntax would call a function?', 
+        answer: '4. function()', 
         choices: [{choice: '1. var function'}, {choice: '2. function'}, {choice: '3. call function'}, {choice: '4. function()'}]
     },
-    {   q: 'When did javascript first appear?', 
-        a: '1. 1995', 
+    {   question: 'When did javascript first appear?', 
+        answer: '1. 1995', 
         choices: [{choice: '1. 1995'}, {choice: '2. Roaring twenties'}, {choice: '3. 2005'}, {choice: '4. 2000'}]
     },
-    {   q: 'What does DOM stand for?', 
-        a: '2. Document Object Model', 
+    {   question: 'What does DOM stand for?', 
+        answer: '2. Document Object Model', 
         choices: [{choice: '1. Do Overnight Modules'}, {choice: '2. Document Object Model'}, {choice: '3. Divas Obviously Model'}, {choice: '4. Do Oo Mo'}]
     },
-    {   q: 'What is getItem commonly used for?', 
-        a: '2. local storage', 
+    {   question: 'What is getItem commonly used for?', 
+        answer: '2. local storage', 
         choices: [{choice: '1. adding drama'}, {choice: '2. local storage'}, {choice: '3. online shopping'}, {choice: '4. naming a variable'}]
     },
 ];
 
-startBtn.addEventListener("click", quizBegin);
 
 
 // Start Timer
 function countdown () {
-    var totalTime = 91;
-
+    var totalTime = 90;
+    
     var timeInterval = setInterval(function() {
-            // As long as the `timeLeft` is greater than 1
+        // As long as the `timeLeft` is greater than 1
         if (totalTime > 1) {
             // Set the `textContent` of `timerEl` to show the remaining seconds
             timer.textContent = totalTime + ' seconds remaining';
@@ -76,51 +75,73 @@ function countdown () {
 };
 
 
-
-
-
 var quizBegin = function () {
     startQuiz.classList.add("hidden");
     questions.classList.remove("hidden");
     countdown ();
+    quizQuestion ();
+};
+//Repeat 7 times
+var quizQuestion = function () {
+    //Pull question from array
+    var currentQuestion = questionsBank[currentQuestionIndex];
+    //Update h1 with current question
+    var choices = document.getElementById("question");
+    choices.textContent = currentQuestion.question;
+    // Clear choices
+    choices.innerHTML = "";
 
+    //loop over questionBank
+    currentQuestion.choices.forEach(function(bank, i) {
+        //generating question choices
+        var bankOption = document.createElement("button");
+        bankOption.setAttribute("class", "bank btn choice");
+        bankOption.setAttribute("value", bank);
 
+        bankOption.textContent = i + 1 + ". " + bank ;
+        //click event for question choices
+        bankOption.onclick = checkAns;
 
-    //Repeat 7 times
-    //for (i = 0;) {
-        //Pull question from array
-        //Listen for choice
-        //Check if choice is correct
-        //Feedback CORRECT or INCORRECT for 2.5 sec
-            //If incorrect then -10 sec
-        //Remove question
-        //Check if time remains
-            //If quiz ends then quizEnd
+        //list choices on screen
+        choices.appendChild(bankOption);
+    });
 
-    //}
 };
 
-var quizEnd = function () {
-    //hide questions
-    //reveal end-of-quiz
-    //button listener
-    // verify name input
-    //send name to storage
-    //got to highScore
+//Listen for choice
+var checkAns = function () {
+    //Check if choice is correct
+    //Feedback CORRECT or INCORRECT for 2.5 sec
+    //If incorrect then -10 sec
 
-
-};
-
-var highScore = function () {
-    //pull from array storage
-    //arrange from high to low score
-    //display scores
-    //listen for button
-    //go to quizBegin
-};
-
-const startScreen = function() {
-    highScores.classList.add("hidden");
-    main.classList.remove("hidden");
-    startQuiz.classList.remove("hidden");
-};
+}
+    //Remove question
+    //Check if time remains
+    //If quiz ends then quizEnd
+    
+    var quizEnd = function () {
+        //hide questions
+        //reveal end-of-quiz
+        //button listener
+        // verify name input
+        //send name to storage
+        //got to highScore
+        
+        
+    };
+    
+    var highScore = function () {
+        //pull from array storage
+        //arrange from high to low score
+        //display scores
+        //listen for button
+        //go to quizBegin
+    };
+    
+    const startScreen = function() {
+        highScores.classList.add("hidden");
+        main.classList.remove("hidden");
+        startQuiz.classList.remove("hidden");
+    };
+    
+startBtn.addEventListener("click", quizBegin);
